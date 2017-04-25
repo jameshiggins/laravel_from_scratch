87fdb1a18dcdb66e2cf30b6ca1c4f8afd4fc92b1
vagrant <vagrant@homestead>
2017-04-25 00:02:28 +0000

    finished lesson 8

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,4 @@
 <?php
 
-use App\Task;
-
-Route::get('/', function () {
-
-    $tasks = Task::all();
-
-    return view('welcome', compact('tasks'));
-});
-
-Route::get('/tasks/{id}', function ($id) {
-
-
-    $tasks = Task::find($id);
-    return view('tasks.show', compact('tasks'));
-
-
-});
-
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{tasks}', 'TasksController@show');
