9152c79c2f9d3b93f8b725e9fdbd734c34ed76f2
vagrant <vagrant@homestead>
2017-05-15 03:01:41 +0000

    end of lesson 15

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,6 +5,19 @@
 
     <h1>{{$post->title}}</h1>
     <p>{{$post->body}}</p>
+    <h4>comments</h4>
+    <div class="comments">
+        <ul class="list-group">
+        @foreach ($post->comments as $comment)
+
+            <div class="list-group-item">
+                <strong>{{$comment->created_at->diffForHumans() }}</strong><br>
+                {{$comment->body}}
+            </div>
+        </ul>
+
+        @endforeach
+    </div>
 </div>
 @endsection
 
