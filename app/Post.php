e1914842f7d3aeb84063b324a3f859a5fcfe4a6f
himjiggins <higginsjamesallen@gmail.com>
2017-05-15 12:44:07 -0700

    extended eloquent model with empty guard

--- app/Post.php
+++ app/Post.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'body'];
+//    protected $fillable = ['title', 'body'];
     public function comments()
     {
         return $this->hasMany(Comment::class);
